ffd308252fb309c82cb1a27c22b6db63710b09a2
Solo <juanoliugomezplatero@gmail.com>
2024-05-10 01:11:09 -0300

    updates payment form with schema

--- frontend/src/PaymentForm.tsx
+++ frontend/src/PaymentForm.tsx
@@ -1,92 +1,91 @@
-import React, { useState, ChangeEvent } from "react";
-
-type PaymentFormFields = {
-  cardNumber: string;
-  expiryMonth: string;
-  expiryYear: string;
-  cvv: string;
-  zipCode: string;
-};
+import React, { ChangeEvent, useState } from "react";
+import { ZodError } from "zod";
+import { FormField } from "./components/FormField";
+import {
+  PaymentFieldErrors,
+  PaymentFormState,
+  paymentFormSchema,
+  paymentFormSchemaInitialValues,
+} from "./schemas";
+import { Separator } from "./components/Separator";
 
 export const PaymentForm: React.FC = () => {
-  const [formData, setFormData] = useState<PaymentFormFields>({
-    cardNumber: "",
-    expiryMonth: "",
-    expiryYear: "",
-    cvv: "",
-    zipCode: "",
-  });
+  const [paymentFormData, setPaymentFormData] = useState<PaymentFormState>(
+    paymentFormSchemaInitialValues
+  );
+  const [paymentFormErrors, setPaymentFormErrors] =
+    useState<PaymentFieldErrors>({});
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    setFormData((prevState) => ({
+    setPaymentFormData((prevState) => ({
       ...prevState,
       [name]: value,
     }));
+
+    setPaymentFormErrors((prev) => ({ ...prev, [name]: undefined }));
   };
 
   const handleSubmit = () => {
-    const paymentData = {
-      cardNumber: formData.cardNumber,
-      expirationDate: `${formData.expiryMonth}/${formData.expiryYear}`,
-      cvv: formData.cvv,
-      zipCode: formData.zipCode,
-    };
+    try {
+      paymentFormSchema.parse(paymentFormData);
+      const paymentData = {
+        cardNumber: paymentFormData.cardNumber,
+        expirationDate: `${paymentFormData.expiryMonth}/${paymentFormData.expiryYear}`,
+        cvv: paymentFormData.cvv,
+        zipCode: paymentFormData.zipCode,
+      };
 
-    window.PaySimple.createToken(paymentData, (result) => {
-      if (result.error) {
-        alert("Error: " + result.error.message);
-      } else {
-        console.log("Token:", result.token);
-        // Send this token to your server for processing.
+      window.PaySimple.createToken(paymentData, (result) => {
+        if (result.error) {
+          alert("Error: " + result.error.message);
+        } else {
+          console.log("Token:", result.token);
+          // Send this token to your server for processing.
+        }
+      });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        // Convert ZodError to a more friendly format
+        const newErrors = error.flatten().fieldErrors;
+        setPaymentFormErrors(newErrors);
       }
-    });
+    }
   };
 
   return (
-    <div className="h-screen flex flex-col space-y-4 p-8">
-      <h1 className="text-2xl font-bold">Payment Form</h1>
-      <input
-        name="cardNumber"
-        value={formData.cardNumber}
-        onChange={handleInputChange}
-        placeholder="Card Number"
-        className="p-2 border border-gray-300 rounded"
-      />
-      <input
-        name="expiryMonth"
-        value={formData.expiryMonth}
-        onChange={handleInputChange}
-        placeholder="Expiry Month (MM)"
-        className="p-2 border border-gray-300 rounded"
-      />
-      <input
-        name="expiryYear"
-        value={formData.expiryYear}
-        onChange={handleInputChange}
-        placeholder="Expiry Year (YYYY)"
-        className="p-2 border border-gray-300 rounded"
-      />
-      <input
-        name="cvv"
-        value={formData.cvv}
-        onChange={handleInputChange}
-        placeholder="CVV"
-        className="p-2 border border-gray-300 rounded"
-      />
-      <input
-        name="zipCode"
-        value={formData.zipCode}
-        onChange={handleInputChange}
-        placeholder="Zip Code"
-        className="p-2 border border-gray-300 rounded"
-      />
-      <button
-        onClick={handleSubmit}
-        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-      >
-        Pay Now
-      </button>
+    <div className="h-screen flex flex-col space-y-8 p-8">
+      <div className="flex flex-col space-y-4">
+        <h1 className="text-2xl font-bold">Merchant Checkout Page</h1>
+        {Object.keys(paymentFormData).map((field) => (
+          <FormField
+            key={field}
+            errors={paymentFormErrors}
+            field={field as keyof PaymentFormState}
+            formfields={paymentFormData}
+            handleInputChange={handleInputChange}
+          />
+        ))}
+      </div>
+      <Separator />
+      <div className="flex flex-col space-y-4">
+        <h1 className="text-2xl font-bold">PaysimpleJS Payment Form</h1>
+        {Object.keys(paymentFormData).map((field) => (
+          <FormField
+            key={field}
+            errors={paymentFormErrors}
+            field={field as keyof PaymentFormState}
+            formfields={paymentFormData}
+            handleInputChange={handleInputChange}
+          />
+        ))}
+        <button
+          onClick={handleSubmit}
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+        >
+          Pay Now
+        </button>
+      </div>
     </div>
   );
 };
